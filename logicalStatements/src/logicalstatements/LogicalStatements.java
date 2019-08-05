5522c76a49f7c6a850178f6c1ca8eb2c930a2a52
matmat-romero <52561165+matmat-romero@users.noreply.github.com>
2019-08-05 10:47:35 +0800

    Add files via upload

---
+++ logicalStatements/src/logicalstatements/LogicalStatements.java
@@ -0,0 +1,25 @@
+
+package logicalstatements;
+import java.util.*;
+
+public class LogicalStatements {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+       
+//        System.out.println("Enter your grade in Math: ");
+//        int gradeMath = input.nextInt();
+//      
+//        System.out.println("Enter your grade in English: ");
+//        int gradeEnglish = input.nextInt();
+//        
+//        if (gradeMath >= 75 || gradeEnglish >= 75 ){
+//            System.out.println("You will pass the year!");
+//        }else{
+//            System.out.println("You will fail");
+//        }
+//        
+        
+                
+    }
+    
+}
