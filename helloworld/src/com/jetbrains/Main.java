4444bea5aa875fe280a130c9e94da7004c029685
matmat-romero <52561165+matmat-romero@users.noreply.github.com>
2019-08-05 10:07:20 +0800

    Add files via upload

---
+++ helloworld/src/com/jetbrains/Main.java
@@ -0,0 +1,28 @@
+package com.jetbrains;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Name:
+        //Age:
+        //Hobbies:
+        //School:
+        //Personal Motto:
+
+
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What's your name: ");
+        String firstname = input.next();
+
+        System.out.println("Your name is: " + firstname);
+
+
+
+
+
+    }
+}
