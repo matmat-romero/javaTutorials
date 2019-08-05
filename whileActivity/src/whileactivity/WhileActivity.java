82858902538f02baf07b3bdba2d2dadc21c6d442
matmat-romero <52561165+matmat-romero@users.noreply.github.com>
2019-08-05 10:59:16 +0800

    Add files via upload

---
+++ whileActivity/src/whileactivity/WhileActivity.java
@@ -0,0 +1,37 @@
+package whileactivity;
+import java.util.*;
+
+public class WhileActivity {
+    public static void main(String[] args) {
+//       Scanner input = new Scanner(System.in);
+//       
+//       while(!"A".equals(letter)){
+//       System.out.print("Enter a letter: ");
+//       String letter = input.next();
+//       }
+        
+//        for (int j = 0; j <= 5; j = j+1){
+//            for (int i = 0; i <= j; i = i + 1){
+//                System.out.print(i);
+//            } 
+//               System.out.println("");
+//        }
+
+
+//    int startingNumber;
+//    
+//    System.out.print("Enter starting number: ");
+//    startingNumber = input.nextInt();
+//                // 1
+//    while(startingNumber <= 5){
+//        
+//        System.out.println(startingNumber);
+//        startingNumber++;
+//    }
+
+
+
+        
+    }
+    
+}
